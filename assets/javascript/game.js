da10c961a0f1f5b490a85ff28ab29f63d1ea3f29
rachel doran <rmitchell0925@gmail.com>
2019-08-03 14:35:10 -0400

    modify

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,4 @@
 var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
-var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
-
-
 
 var wins = 0;
 var losses = 0;
@@ -12,66 +9,46 @@ var guessesSoFar = [];
 var winsPlayer = document.getElementById("wins");
 var lossesPlayer = document.getElementById("losses");
 var guessesLeftPlayer = document.getElementById("guesses-left");
-var guessesFar = document.getElementById("guesses-so-far");
+var guessesRemaining = document.getElementById("guesses-so-far");
 
-
-
-
+var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+console.log(computerGuess);
 
 document.onkeyup = function (event) {
   var userGuess = event.key;
   // console.log(userGuess);
 
-  var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-  console.log('computer guess',computerGuess);
+  guessesLeft--;
+  guessesSoFar.push(userGuess);
+  console.log(guessesLeft);
 
-  // if(computerGuess === userGuess){
-  //   console.log('matches')
-  // }else{
-  //   console.log('does not matches')
-  // }
-  // if (computerChoices== -1) {
-  //   console.log('computerGuess',computerGuess);
-  // }else {
-  //   console.log('its founnd')
-  // }
 
-    if (userGuess === computerGuess) {
-      wins++;
-      guessesLeft--;
-      console.log("guessesleft",guessesLeft)
-      guessesSoFar.push(userGuess);
-      winsPlayer.innerHTML = "Wins: " + wins;
-      guessesLeftPlayer.innerHTML = "GuessLeft: " + guessesLeft;
-      console.log("user guess",guessesSoFar);
+  if (userGuess === computerGuess) {
+    wins++;
+    guessesLeft = 9;
+    guessesSoFar = [];
+    computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    console.log(computerGuess);
+    
 
-    }else{
-      guessesLeft--;
-      console.log(guessesLeft)
-      guessesLeftPlayer.innerHTML = "GuessLeft: " + guessesLeft;
-    }
-    if(guessesLeft === 0){
-      alert("gameOver")
-      guessesLeft = 9
-    }
 
-    if (userGuess !== computerGuess) {
-      guessesLeft--;
-      guessesSoFar.push(userGuess);
-      console.log(guessesSoFar);
-      guessesSoFar.innertext = "Your Guesses So Far: " + guessesSoFar;
-    }
-
-   if (guessesLeft === 0) {
-      guessesLeft = 9;
-      losses++;
-      guessesSoFar.push(userGuess);
-      lossesPlayer.innerHTML = "Losses: " + losses;
-    guessesLeftPlayer.innerHTML = guessesSoFar;
-      
-
-   }
+  } else if (guessesLeft === 0) {
+    alert("Game Over");
+    guessesLeft = 9;
+    guessesSoFar= [];
+    console.log(guessesSoFar);
+    losses++;
+    computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    console.log(computerGuess);
+  
   }
+  winsPlayer.innerHTML = "Wins: " + wins;
+  lossesPlayer.innerHTML = "Losses: " + losses;
+  guessesLeftPlayer.innerHTML = "GuessLeft: " + guessesLeft;
+  guessesRemaining.innerHTML = "Your Guesses So Far: " + guessesSoFar;
+
+}
+
 
 
 
